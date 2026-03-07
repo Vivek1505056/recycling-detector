8af9dab4b58e8e7ea50642254d085c1b4d979ec2
Vivek Koul <vivekkoul@eduroam-campus-10-36-4-124.campus-dynamic.uwaterloo.ca>
2026-03-07 15:50:30 -0500

    initial commit fr

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,21 @@
 export default {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      content: ['./index.html', './src/**/*.{ts,tsx}'],
+      theme: {
+        extend: {
+          colors: {
+            brand: {
+              ink: '#17301c',
+              moss: '#6e9f4d',
+              cream: '#f6f3ea',
+              clay: '#d7c1a2',
+            },
+          },
+        },
+      },
+      plugins: [],
+    },
     autoprefixer: {},
   },
 };
-
